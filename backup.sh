cb25df3319aa4a5855f3e54c54358a141b5996a1
serg6241 <regs6240@gmail.com>
2015-10-18 21:31:25 +0300

    zz

--- backup.sh
+++ backup.sh
@@ -13,8 +13,8 @@
  mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
 # Delete files older than 30 days
 # find $backup_path/* -mtime +30 -exec rm {} \;
-
-/bin/tar czvf /var/www/shared-$date.tar.gz /var/www/html/wp-content/uploads/ /var/www/html/wp-content/upgrade /var/www/html/wp-config.php
+cd /var/www/
+/bin/tar czvf /var/www/shared-$date.tar.gz html/wp-content/uploads/ html/wp-content/upgrade html/wp-config.php
 
 aws s3 cp /var/www/shared.tar.gz s3://wordpres-shared
 aws s3 cp /var/www/html/wordpress.sql s3://wordpres-shared
