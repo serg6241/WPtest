7c4122823e8b66a7725db53167305e8490bfbf14
serg6241 <regs6240@gmail.com>
2015-10-18 21:17:21 +0300

    Update backup script

--- backup.sh
+++ backup.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -xe
-/bin/tar czvf /var/www/shared.tar.gz /var/www/html/wp-content/uploads/ /var/www/html/wp-content/upgrade /var/www/html/wp-config.php
+# Database credentials
+ user="root"
+ password="cb7ebb"
+ host="localhost"
+ db_name="wordpress"
+# Other options
+ backup_path="/var/www/html"
+ date=$(date +"%d-%b-%Y")
+# Set default file permissions
+ umask 177
+# Dump database into SQL file
+ mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
+# Delete files older than 30 days
+# find $backup_path/* -mtime +30 -exec rm {} \;
+
+/bin/tar czvf /var/www/shared-$date.tar.gz /var/www/html/wp-content/uploads/ /var/www/html/wp-content/upgrade /var/www/html/wp-config.php
+
+aws s3 cp /var/www/shared-$date.tar.gz s3://wordpres-shared
+aws s3 cp /var/www/html/$db_name-$date.sql s3://wordpres-shared
